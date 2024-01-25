2c16b21f025edb4fb430804ab50e6d22029045d0
Nick Berendsen <58348935+Desbeers@users.noreply.github.com>
2024-01-25 20:08:11 +0100

    Add option to group a single movie into a set

--- Sources/SwiftlyKodiAPI/GlobalTypes/Video/Video+Details+Movie+Extensions (SwiftlyKodi).swift
+++ Sources/SwiftlyKodiAPI/GlobalTypes/Video/Video+Details+Movie+Extensions (SwiftlyKodi).swift
@@ -28,17 +28,33 @@ extension Array where Element == Video.Details.Movie {
     /// - Returns: An array of ``KodiItem``
     /// - Parameter host: The ``HostItem`` that has the movie sets
     public func swapMoviesForSet(host: HostItem) async -> [any KodiItem] {
+        /// Check if the setting to group movies into sets is enabled
         if await Settings.getSettingValue(
             host: host,
             setting: .videolibraryGroupMovieSets
         ).boolean {
-            let moviesBySet = Dictionary(grouping: self) { item in
-                item.setID
-            }
+            /// Get all movie sets
             var movieSets = await VideoLibrary.getMovieSets(host: host)
-            let movieSetIDs = moviesBySet.filter { $0.value.count > 1 } .map {$0.key}
-            movieSets = movieSets.filter { movieSetIDs.contains($0.setID) }
-            return ((moviesBySet[0] ?? []) + movieSets)
+            /// Check if a set with just one movie should be grouped
+            let singleItem = await Settings.getSettingValue(
+                host: host,
+                setting: .videolibraryGroupSingleItemSets
+            ).boolean
+            switch singleItem {
+            case true:
+                let movieSetIDs = Set(self.map(\.setID))
+                movieSets = movieSets.filter { movieSetIDs.contains($0.setID) }
+                return (self.filter { $0.setID == 0 } + movieSets)
+            case false:
+                var moviesBySet = Dictionary(grouping: self) { item in
+                    item.setID
+                }
+                /// Remove setID 0, because it means 'not part of a set'
+                moviesBySet.updateValue([], forKey: 0)
+                let movieSetIDs = moviesBySet.filter { $0.value.count > 1 } .map {$0.key}
+                movieSets = movieSets.filter { movieSetIDs.contains($0.setID) }
+                return (self.filter { !movieSetIDs.contains($0.setID) } + movieSets)
+            }
         } else {
             return self
         }
